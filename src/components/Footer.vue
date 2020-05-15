ce162bb01362fc32649b0ba293816c60d523d349
kelvin2408 <lstskynet123@gmail.com>
2020-05-15 16:49:57 +0700

    third commit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
-<div class="row-footer">
+<div class="footer">
     <div class="col-md-12 mt-5">
-        <p class="footer">&copy;2020 by Kelvin Irawan</p>
+        <p class="footer-boos">&copy;2020 by Kelvin Irawan</p>
         <p class="footer-sub">All Rights Reserved</p>
     </div>
 </div>
@@ -12,19 +12,24 @@
 export default {
 }
 </script>
+
 <style>
-.row-footer {
-    width: 100%;
-    margin: 0px;
-    padding: 30px;
-    text-align: center;
-    background-color: rgb(86, 175, 171);
-}
 .footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color : rgb(86, 175, 171);
+   color: white;
+   text-align: center;
+}
+
+.footer-boos {
     font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
     Roboto, "Helvetica Neue", Arial, sans-serif;
     color: #ffffff;
 }
+
 .footer-sub {
     font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
     Roboto, "Helvetica Neue", Arial, sans-serif;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row row-about p-5">
             <div class="col-md-6 p-4">
-                <img src="../assets/foto.jpg" width="320px" height="320px"/>
+                <img src="../assets/foto.jpg" width="290px" height="320px"/>
             </div>
             <div class="col-md-6 p-4">
                 <h1 class="mb-5">About Me<span class="text-danger">.</span></h1>
@@ -12,7 +12,29 @@
                     disini kami menawarkan cara yang mudah untuk medapatkanya
                     silahkan hubungi di contact person
                 </p>
+                <div class="chip">
+                   <img src="../assets/wa.png" alt="Person" width="96" height="96"> 08123456778 </div>
             </div>
         </div>
     </div>
 </template>
+
+<style>
+.chip {
+  display: inline-block;
+  padding: 0 25px;
+  height: 50px;
+  font-size: 16px;
+  line-height: 50px;
+  border-radius: 25px;
+  background-color: #f1f1f1;
+}
+
+.chip img {
+  float: left;
+  margin: 0 10px 0 -25px;
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+}
+</style>
